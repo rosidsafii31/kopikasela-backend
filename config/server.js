e5da00c1ca84a925c06071104505fc4c671535c0
MohNurSafii <68380642+rosidsafii31@users.noreply.github.com>
2021-12-17 09:57:37 +0700

    base

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5ec2ae143128526ffeb63c8743f78bdf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1e1b912f-10f7-4612-8669-b14781a535c3'
+};
